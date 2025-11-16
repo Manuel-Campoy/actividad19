d03b40b5d89db58d8f387c868603b1a5623c586a
Manuel-Campoy <mcampoyteran5@gmail.com>
2025-11-15 17:56:23 -0700

    video8 start

---
+++ video8/react-youtube/src/components/VideoList.jsx
@@ -0,0 +1,12 @@
+import styles from './VideoList.module.css';
+
+const VideoList = ({ title, children }) => 
+    (
+        <div className={`${styles.wrapperList} ${styles.margin}`}>
+            <h2>{title}</h2>
+            {children || <p>No hay videos</p>}
+        </div>
+    )
+
+
+export default VideoList
