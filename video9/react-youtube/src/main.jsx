619b3f53d47f8238a752dc67e3cb16ab9d5d20d5
Manuel-Campoy <mcampoyteran5@gmail.com>
2025-11-21 11:48:06 -0700

    fix main

--- video9/react-youtube/src/main.jsx
+++ video9/react-youtube/src/main.jsx
@@ -1,7 +1,18 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
+import React from 'react';
 
 const container = document.getElementById('root');
 
-ReactDOM.render(<App />, container);
+if (container) {
+  const root = ReactDOM.createRoot(container);
+  
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error("The root container element ('root') was not found in the DOM.");
+}
